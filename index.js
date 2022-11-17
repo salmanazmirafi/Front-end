d230eef09965cdd5337f0f22e2984028840cc77e
Salman Azmi Rafi <salmanazmirafipersonal@gmail.com>
2022-11-17 22:58:24 +0600

    last

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 const userRoutes = require("./Routes/userRoutes");
 
 dotenv.config();
+app.use(morgan("common"));
 app.use("/", userRoutes);
 
 mongoose.connect(process.env.DATABASE_URI, () => {
