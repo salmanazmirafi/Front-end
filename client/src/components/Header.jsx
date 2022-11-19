426bc9090278039e5b30a784eb4af25e97a14db9
Salman Azmi Rafi <salmanazmirafipersonal@gmail.com>
2022-11-19 21:10:39 +0600

    some Update

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,31 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Toolbar, Typography, Box, Button } from "@mui/material";
 
 function Header() {
   return (
-    <AppBar>
+    <AppBar
+      sx={{
+        background:
+          "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)",
+      }}
+    >
       <Toolbar>
-        <Typography>Hi</Typography>
+        <Typography variant="h3">Blog</Typography>
+        <Box display="flex" marginLeft="auto">
+          <Button
+            variant="contained"
+            sx={{ margin: 1, borderRadius: 2 }}
+            color="warning"
+          >
+            Login
+          </Button>
+          <Button
+            variant="contained"
+            sx={{ margin: 1, borderRadius: 2 }}
+            color="warning"
+          >
+            Sign Up
+          </Button>
+        </Box>
       </Toolbar>
     </AppBar>
   );
