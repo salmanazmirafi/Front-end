cc1005bf5fdc8473b3b5b3dce6d7c5107c629af9
Salman Azmi Rafi <salmanazmirafipersonal@gmail.com>
2022-11-21 03:03:41 +0600

    last

--- client/src/App.js
+++ client/src/App.js
@@ -7,17 +7,34 @@ import MyBlog from "./components/MyBlog";
 import BlogsDetlass from "./components/BlogsDetlass";
 import AddBlog from "./components/AddBlog";
 
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { authActions } from "./store";
 function App() {
+  const dispath = useDispatch();
+
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+  console.log(isLoggedIn);
+  useEffect(() => {
+    if (localStorage.getItem("user")) {
+      dispath(authActions.login());
+    }
+  }, [dispath]);
   return (
     <>
       <Header />
       <main>
         <Routes>
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/my-blog" element={<MyBlog />} />
-          <Route path="/my-blog/:id" element={<BlogsDetlass />} />
-          <Route path="/blog/add" element={<AddBlog />} />
+          {!isLoggedIn ? (
+            <Route path="/auth" element={<Auth />} />
+          ) : (
+            <>
+              <Route path="/blogs" element={<Blogs />} />
+              <Route path="/my-blog" element={<MyBlog />} />
+              <Route path="/my-blog/:id" element={<BlogsDetlass />} />
+              <Route path="/blog/add" element={<AddBlog />} />
+            </>
+          )}
         </Routes>
       </main>
     </>
--- client/src/components/BlogsDetlass.jsx
+++ client/src/components/BlogsDetlass.jsx
@@ -1,11 +1,23 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { Box } from "@mui/system";
+import { Button, InputLabel, TextField, Typography } from "@mui/material";
+const labelStyles = { mb: 1, mt: 2, fontSize: "24px", fontWeight: "bold" };
 
 function BlogsDetlass() {
+  const navigate = useNavigate();
   const [blog, setBlog] = useState();
   const id = useParams().id;
   console.log(id);
+  const [inputs, setInputs] = useState({});
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
   const faceUser = async () => {
     const res = await axios
       .get(`http://localhost:4000/api/blog/${id}`)
@@ -15,11 +27,101 @@ function BlogsDetlass() {
   };
 
   useEffect(() => {
-    faceUser().then((data) => setBlog(data.Id));
+    faceUser().then((data) => {
+      setBlog(data.Id);
+      setInputs({
+        title: data.Id.title,
+        description: data.Id.description,
+        imageURL: data.Id.image,
+      });
+    });
   }, [id]);
   console.log(blog);
 
-  return <div>BlogsDetlass</div>;
+  const sendRequest = async () => {
+    const res = await axios
+      .put(`http://localhost:4000/api/blog/${id}`, {
+        title: inputs.title,
+        description: inputs.description,
+        image: inputs.imageURL,
+      })
+      .catch((err) => console.log(err));
+
+    const data = await res.data;
+    return data;
+  };
+  console.log(blog);
+  const handaleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+    sendRequest()
+      .then((data) => console.log(data))
+      .then(() => navigate("/blogs/"));
+  };
+
+  return (
+    <div>
+      {" "}
+      {inputs && (
+        <form onSubmit={handaleSubmit}>
+          <Box
+            border={3}
+            borderColor="linear-gradient(90deg, rgba(58,75,180,1) 2%, rgba(116,49,110,1) 36%, rgba(2,0,161,1) 73%, rgba(69,92,252,1) 100%)"
+            borderRadius={10}
+            boxShadow="10px 10px 20px #ccc"
+            padding={3}
+            margin={"auto"}
+            marginTop={3}
+            display="flex"
+            flexDirection={"column"}
+            width={"80%"}
+          >
+            <Typography
+              fontWeight={"bold"}
+              padding={3}
+              color="grey"
+              variant="h2"
+              textAlign={"center"}
+            >
+              Update Your Blog
+            </Typography>
+            <InputLabel sx={labelStyles}>Title</InputLabel>
+            <TextField
+              onChange={handleChange}
+              value={inputs.title}
+              name="title"
+              margin="auto"
+              variant="outlined"
+            />
+            <InputLabel sx={labelStyles}>Description</InputLabel>
+            <TextField
+              onChange={handleChange}
+              value={inputs.description}
+              name="description"
+              margin="auto"
+              variant="outlined"
+            />
+            <InputLabel sx={labelStyles}>ImageURL</InputLabel>
+            <TextField
+              onChange={handleChange}
+              value={inputs.imageURL}
+              name="imageURL"
+              margin="auto"
+              variant="outlined"
+            />
+            <Button
+              sx={{ mt: 2, borderRadius: 4 }}
+              variant="contained"
+              color="warning"
+              type="submit"
+            >
+              Submit
+            </Button>
+          </Box>
+        </form>
+      )}
+    </div>
+  );
 }
 
 export default BlogsDetlass;
