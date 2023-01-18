8253276a8fe420977b1887fc50b8b44d7e067fb0
salmanazmirafi <116106498+salmanazmirafi@users.noreply.github.com>
2023-01-18 23:16:12 +0600

    Update

--- Routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const { getAlluser, createUser, login } = require("../controlar/userCon");
-
-const userRoutes = require("express").Router();
-
-userRoutes.get("/", getAlluser);
-userRoutes.post("/signup", createUser);
-userRoutes.post("/login", login);
-
-module.exports = userRoutes;
